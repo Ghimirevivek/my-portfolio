1e3d3f925aa3a24aa1e20264724e8f1e9e0f2deb
Vivek Ghimire <108508449+Ghimirevivek@users.noreply.github.com>
2024-11-28 17:29:53 +0530

    globe

--- components/ui/GridGlobe.tsx
+++ components/ui/GridGlobe.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import dynamic from 'next/dynamic';
 
-const World = dynamic(() => import('../ui/Globe').then((m) => m.World), {
+const World = dynamic(() => import('../ui/globe').then((m) => m.World), {
   ssr: false,
 });
 
@@ -395,8 +395,9 @@ export function GlobeDemo() {
   ];
 
   return (
-    <div className='flex flex-row items-center justify-center relative w-full'>
-      {/* <motion.div
+    <div className='flex flex-row items-center justify-center py-20 h-screen md:h-auto dark:bg-black bg-white relative w-full'>
+      <div className='max-w-7xl mx-auto w-full relative overflow-hidden h-full md:h-[40rem] px-4'>
+        <motion.div
           initial={{
             opacity: 0,
             y: 20,
@@ -415,13 +416,13 @@ export function GlobeDemo() {
           </h2>
           <p className='text-center text-base md:text-lg font-normal text-neutral-700 dark:text-neutral-200 max-w-md mt-2 mx-auto'>
             This globe is interactive and customizable. Have fun with it, and
-            don&apos;t forget to share it. :
+            don&apos;t forget to share it. :)
           </p>
-          bg-gradient-to-b from-transparent dark:to-black to-white
-        </motion.div> */}
-      {/* <div className='absolute w-full bottom-0 inset-x-0 h-40 pointer-events-none select-none z-40' /> */}
-      <div className='absolute -top-[26rem] left-[11rem] w-full h-[28rem] z-10'>
-        <World data={sampleArcs} globeConfig={globeConfig} />
+        </motion.div>
+        <div className='absolute w-full bottom-0 inset-x-0 h-40 bg-gradient-to-b pointer-events-none select-none from-transparent dark:to-black to-white z-40' />
+        <div className='absolute w-full -bottom-20 h-72 md:h-full z-10'>
+          <World data={sampleArcs} globeConfig={globeConfig} />
+        </div>
       </div>
     </div>
   );
